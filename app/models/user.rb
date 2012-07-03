3e5d08a3b2a0ef3ff9ebc67b55c7b3fcfc3a7066
Tiago de Assis Santos <tiago@casa.(none)>
2012-07-02 22:48:09 -0300

    Novo favicon, icones para indicação de tipo de estória

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :id, :email, :password, :password_confirmation, :remember_me, :papel_id
+  attr_accessible :id, :email, :password, :password_confirmation, :remember_me, :papel_id, :nome
   
   #has_and_belongs_to_many :projetos
   has_many :permissoes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120329001908) do
+ActiveRecord::Schema.define(:version => 20120607201253) do
 
   create_table "anexos", :force => true do |t|
     t.string   "arquivo"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(:version => 20120329001908) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nome"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
